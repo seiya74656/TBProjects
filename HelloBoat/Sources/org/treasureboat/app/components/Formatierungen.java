42c612194fdf001d587923dab9486ef63d6fbe0e
Ken ISHIMOTO <ken@ksroom.com>
2014-10-07 08:06:06 +0200

    Format + remove TBNonSynchronizing
    für Testen brauchst du das noch nicht

--- HelloBoat/Sources/org/treasureboat/app/components/Formatierungen.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Formatierungen.java
@@ -1,6 +1,5 @@
 package org.treasureboat.app.components;
 
-import org.treasureboat.webcore.annotations.TBNonSynchronizing;
 import org.treasureboat.webcore.annotations.TBPageAccess;
 import org.treasureboat.webcore.components.TBComponent;
 
@@ -10,25 +9,27 @@ import com.webobjects.foundation.NSTimestamp;
 @TBPageAccess (
     navigationState = "Welcome"
     )
-@TBNonSynchronizing
-
 public class Formatierungen extends TBComponent {
+  /**
+   * Do I need to update serialVersionUID?
+   * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
+   * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
+   */
   private static final long serialVersionUID = 1L;
 
-	//********************************************************************
-	//  Constructor : コンストラクタ
-	//********************************************************************
+  //********************************************************************
+  //  Constructor : コンストラクタ
+  //********************************************************************
 
-	public Formatierungen(WOContext context) {
-		super(context);
-	}
+  public Formatierungen(WOContext context) {
+    super(context);
+  }
 
-	//********************************************************************
+  //********************************************************************
   //  Methods : メソッド
   //********************************************************************
-public NSTimestamp timestamp(){
-  return new NSTimestamp();
-}
-
+  public NSTimestamp timestamp(){
+    return new NSTimestamp();
+  }
 
 }
