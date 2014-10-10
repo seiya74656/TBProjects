a84190a0a6fa1178ea3ec7542601d78cbc908f9b
Rene Fuerst <seiya@me.com>
2014-10-10 10:44:47 +0200

    Testing TBFont with multiple selector

--- HelloBoat/Sources/org/treasureboat/app/components/Formatierungen.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Formatierungen.java
@@ -1,5 +1,6 @@
 package org.treasureboat.app.components;
 
+import org.treasureboat.foundation.TBFString;
 import org.treasureboat.webcore.annotations.TBPageAccess;
 import org.treasureboat.webcore.components.TBComponent;
 
@@ -35,40 +36,48 @@ public class Formatierungen extends TBComponent {
   //********************************************************************
 
   // Counter GET UND SET
-  
+
   public void setcounter(int counter) {
-   this.counter = counter+1;
+    this.counter = counter+1;
   } 
 
   public int getcounter() {
-   return counter;
- }
-  
+    return counter;
+  }
+
   public int counter;  
 
   // Counter GET UND SET END
-  
-//Usernames GET UND SET
-  
- public void setusernames(String username) {
-  if (username.equals("seiya")) { usernames = "OK"; } else { usernames = "NOK";}
- } 
 
- public String getusernames() {
-  return usernames;
-}
- 
- // Username GET UND SET END
- 
+  //Usernames GET UND SET
+
+  public void setusernames(String username) {
+    if (TBFString.stringIsNullOrEmpty(username)) {
+      username = "No Name";
+    }
+
+    if (username.equalsIgnoreCase("seiya")) { 
+      usernames = "OK"; 
+    } else { 
+      usernames = "NOK";
+    }
+  } 
+
+  public String getusernames() {
+    return usernames;
+  }
+
+  // Username GET UND SET END
+
   public WOActionResults doSubmitAction() {
     setcounter(counter);
     setusernames(username);
     System.err.println( getcounter() + getusernames() );
     System.err.println("sampleText = " + sampleText + "\n Username = " + username + "\n Password" + passwords);
-    
+
     return goToMySelfAction();
   }
-  
+
   public WOActionResults goBackToMainAction() {
     Main nextPage = pageWithName(Main.class);
     return nextPage;
