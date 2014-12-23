eb90568c2e572eb47b3cc1f9757df91b9c0ae51c
Rene Fuerst <seiya@me.com>
2014-12-23 15:57:34 +0100

    Add Aninite

---
+++ HelloBoat/Sources/org/treasureboat/app/components/Aninite.java
@@ -0,0 +1,16 @@
+package org.treasureboat.app.components;
+
+import org.treasureboat.webcore.components.TBComponent;
+
+import com.webobjects.appserver.WOContext;
+
+public class Aninite extends TBComponent {
+    /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+    public Aninite(WOContext context) {
+        super(context);
+    }
+}
