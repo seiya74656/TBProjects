3038fa024859f0aa59bd0f5d85c8c627af1cc52d
Rene Fuerst <seiya@me.com>
2014-12-25 20:42:06 +0100

    makeup

--- HelloBoat/Sources/org/treasureboat/app/components/Calendar.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Calendar.java
@@ -10,11 +10,7 @@ import com.webobjects.appserver.WOContext;
     )
 
 public class Calendar extends TBComponent {
-  /**
-   * Do I need to update serialVersionUID?
-   * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
-   * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
-   */
+
   private static final long serialVersionUID = 1L;
 
   //********************************************************************
@@ -24,4 +20,9 @@ public class Calendar extends TBComponent {
     public Calendar(WOContext context) {
         super(context);
     }
+    
+    
+        
+    
+    
 }
