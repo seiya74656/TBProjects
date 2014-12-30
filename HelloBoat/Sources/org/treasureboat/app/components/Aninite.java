8e4ac0e20602af28b7d78cbd300091ecf86bf75b
Rene Fuerst <seiya@me.com>
2014-12-30 15:23:55 +0100

    Learning Wonder Page 110, test Persons, not working currently.

--- HelloBoat/Sources/org/treasureboat/app/components/Aninite.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Aninite.java
@@ -4,7 +4,7 @@ import org.treasureboat.webcore.components.TBComponent;
 
 import com.webobjects.appserver.WOContext;
 import com.webobjects.foundation.NSArray;
-import com.webobjects.foundation.NSTimestamp;
+import com.webobjects.foundation.NSMutableArray;
 
 public class Aninite extends TBComponent {
     /**
@@ -16,10 +16,40 @@ public class Aninite extends TBComponent {
         super(context);
         }
 
-    public NSTimestamp mytimestamp = new NSTimestamp();
-//Test of NSArray
-    public NSArray<String> monate = new NSArray<String>(new String[] { "januar", "februar", "märz", "april", "mai", "juni", "juli", "august",
-        "september", "oktober", "november", "dezember" });
+    public class Personen {
+      private String vorName;
+      private String nachName;
+      
+      public Personen(String vorName, String nachName){
+        super();
+        this.vorName = vorName;
+        this.nachName = nachName;        
+      }
+      
+      public String nachName(){
+        return nachName;
+      }
+      
+      public void setnachName(String nachName) {
+        this.nachName = nachName;
+      }
+      
+      public String vorName(){
+        return vorName;
+      }
+      
+      public void setvorName(String vorName) {
+        this.vorName = vorName;
+      }
+      
+    }
+    
+    private NSMutableArray<Personen> persons;
 
+    persons = new NSMutableArray<Personen>();
+    persons.add(new Personen("Rene", "Fuerst"));
 
-}
+    public NSMutableArray<Personen> persons() {
+      return persons;
+    }
+}    
