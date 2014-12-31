ae6ae80340c16915c694c08506f93d3f9257ae6e
Rene Fuerst <seiya@me.com>
2014-12-31 15:21:55 +0100

    add personloop

--- HelloBoat/Sources/org/treasureboat/app/components/Aninite.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Aninite.java
@@ -12,17 +12,30 @@ public class Aninite extends TBComponent {
 
   private static final long serialVersionUID = 1L;
   private NSMutableArray<Personen> persons;
-
+  private Personen personenloop;
+  
   public Aninite(WOContext context) {
     super(context);
 
 
-
+// fill array
+    
     persons = new NSMutableArray<Personen>();
-    persons.add(new Personen("Lolo", "lala"));
+    persons.add(new Personen("Hello", "World"));
     persons.add(new Personen("Rene", "Fuerst"));
 
     public NSMutableArray<Personen> persons() {
       return persons;
     }
+    
+// Personen loop
+    public Personen personenloop() {
+    return personenloop;  
+    }
+    
+    public void setpersonenloop(Person persons) {
+      this.personenloop = personenloop;      
+    }
+    
+    
   }    
