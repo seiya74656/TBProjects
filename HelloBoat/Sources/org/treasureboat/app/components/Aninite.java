34388f021bb691049b9a1a927f2210a19ae895e1
Rene Fuerst <seiya@me.com>
2014-12-31 15:02:27 +0100

    Error second "persons.add"

--- HelloBoat/Sources/org/treasureboat/app/components/Aninite.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Aninite.java
@@ -1,55 +1,28 @@
 package org.treasureboat.app.components;
 
 import org.treasureboat.webcore.components.TBComponent;
+import org.treasureboat.app.components.Personen;
 
 import com.webobjects.appserver.WOContext;
 import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSMutableArray;
 
+
 public class Aninite extends TBComponent {
-    /**
-   * 
-   */
+
   private static final long serialVersionUID = 1L;
+  private NSMutableArray<Personen> persons;
 
-    public Aninite(WOContext context) {
-        super(context);
-        }
+  public Aninite(WOContext context) {
+    super(context);
 
-    public class Personen {
-      private String vorName;
-      private String nachName;
-      
-      public Personen(String vorName, String nachName){
-        super();
-        this.vorName = vorName;
-        this.nachName = nachName;        
-      }
-      
-      public String nachName(){
-        return nachName;
-      }
-      
-      public void setnachName(String nachName) {
-        this.nachName = nachName;
-      }
-      
-      public String vorName(){
-        return vorName;
-      }
-      
-      public void setvorName(String vorName) {
-        this.vorName = vorName;
-      }
-      
-    }
-    
-    private NSMutableArray<Personen> persons;
+
 
     persons = new NSMutableArray<Personen>();
+    persons.add(new Personen("Lolo", "lala"));
     persons.add(new Personen("Rene", "Fuerst"));
 
     public NSMutableArray<Personen> persons() {
       return persons;
     }
-}    
+  }    
---
+++ HelloBoat/Sources/org/treasureboat/app/components/Personen.java
@@ -0,0 +1,28 @@
+package org.treasureboat.app.components;
+
+public class Personen {
+  private String vorName;
+  private String nachName;
+
+  public Personen(String vorName, String nachName){
+    this.vorName = vorName;
+    this.nachName = nachName;        
+  }
+
+  public String nachName(){
+    return nachName;
+  }
+
+  public void setnachName(String nachName) {
+    this.nachName = nachName;
+  }
+
+  public String vorName(){
+    return vorName;
+  }
+
+  public void setvorName(String vorName) {
+    this.vorName = vorName;
+  }
+
+}
