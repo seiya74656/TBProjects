a555c27147a834f7df74b444f98dcecf4053ff5b
Rene Fuerst <seiya@me.com>
2015-01-02 22:42:13 +0100

    Adventkalendar Array

---
+++ HelloBoat/Sources/org/treasureboat/app/components/Adventkalendar.java
@@ -0,0 +1,44 @@
+package org.treasureboat.app.components;
+
+public class Adventkalendar {
+
+  public Adventkalendar (String tag, String code, String frage) {
+  super();
+  
+  _tag = tag;
+  _code = code;
+  _frage = frage;
+  
+  }
+  
+
+  public void setTag(String tag) {
+    _tag = tag;
+  }
+  public String tag() {
+    return _tag;
+  }
+  private String _tag;
+
+  public String code() {
+    return _code;
+  }
+
+  public void setCode(String code) {
+    _code = code;
+  }
+
+  private String _code;
+
+  
+  public String frage() {
+    return _frage;
+  }
+
+  public void setFrage(String frage) {
+    _frage = frage;
+  }
+  
+  private String _frage;
+  
+}
--- HelloBoat/Sources/org/treasureboat/app/components/MyCalendar.java
+++ HelloBoat/Sources/org/treasureboat/app/components/MyCalendar.java
@@ -4,6 +4,7 @@ import org.treasureboat.webcore.annotations.TBPageAccess;
 import org.treasureboat.webcore.components.TBComponent;
 
 import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSMutableArray;
 
 @TBPageAccess (
     navigationState = "Calendar"
@@ -19,4 +20,34 @@ public class MyCalendar extends TBComponent {
     public MyCalendar(WOContext context) {
         super(context);
     }
+    public NSMutableArray<Adventkalendar> adventkalendar() {
+      adventkalendar = new NSMutableArray<Adventkalendar>();
+      adventkalendar.add(new Adventkalendar("1", "Lala", "MMM"));
+      adventkalendar.add(new Adventkalendar("2", "Papa", "ddd"));
+      adventkalendar.add(new Adventkalendar("3", "Mama", "fff"));
+      adventkalendar.add(new Adventkalendar("4", "DaDa", "ggg"));
+      adventkalendar.add(new Adventkalendar("5", "KaKa", "eee"));
+      adventkalendar.add(new Adventkalendar("6", "HaHa", "Lolo"));
+      adventkalendar.add(new Adventkalendar("7", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("8", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("9", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("11", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("12", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("13", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("14", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("15", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("16", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("17", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("18", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("19", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("20", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("21", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("22", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("23", "Lala", "Lolo"));
+      adventkalendar.add(new Adventkalendar("24", "Lala", "Lolo"));
+      return adventkalendar;
+    }
+    
+    private NSMutableArray<Adventkalendar> adventkalendar;
+    public Adventkalendar adventkalendarloop;
 }
