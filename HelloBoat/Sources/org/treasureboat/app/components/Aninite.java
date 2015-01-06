54734f1e21ca1e9217b103854ee3af55ce4de5c0
Rene Fuerst <seiya@me.com>
2015-01-06 19:55:36 +0100

    log für randomzahl, java math random

--- HelloBoat/Sources/org/treasureboat/app/components/Aninite.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Aninite.java
@@ -8,7 +8,7 @@ import com.webobjects.foundation.NSMutableArray;
 public class Aninite extends TBComponent {
 
   private static final long serialVersionUID = 1L;
-  
+
   public Aninite(WOContext context) {
     super(context);
   }
@@ -19,18 +19,23 @@ public class Aninite extends TBComponent {
       persons.add(new Personen("Ken", "Ishimoto"));
       persons.add(new Personen("Renate", "Fürst"));
       persons.add(new Personen("Rene", "Fürst"));
+      persons.add(new Personen("Bettina", "Lebiu"));
+      persons.add(new Personen("Jenny", "Lebiu"));
+      persons.add(new Personen("Pietro", "Lebiu"));
     }
     return persons;
   }
   private NSMutableArray<Personen> persons;
   public Personen personenloop;
-    
+
   public Personen secondPerson() {
     if (persons() == null || persons().count() < 2) { 
       return null;
     }
-    return persons().objectAtIndex((int)Math.random()*persons().count());
+    int zufallszahl = (int)(Math.random()*persons().count());
+    System.err.println(zufallszahl);
+    return persons().objectAtIndex(zufallszahl);
   }
-  
-  
+
+
 }    
