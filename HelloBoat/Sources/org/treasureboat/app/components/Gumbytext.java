6a49a623ae405c29fa3bd431707a1d2bd1ef3c6c
Rene Fuerst <seiya@me.com>
2015-01-08 11:39:25 +0100

    Add some TB Form Testings
    Some errors with TBJSAlertPanel

--- HelloBoat/Sources/org/treasureboat/app/components/Gumbytext.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Gumbytext.java
@@ -1,44 +1,103 @@
 package org.treasureboat.app.components;
 
 import com.webobjects.appserver.WOContext;
-import org.treasureboat.webcore.components.TBComponent;
-import com.webobjects.appserver.WOActionResults;
 
+import org.treasureboat.webcore.components.TBComponent;
+
+import com.webobjects.appserver.WOActionResults;
+import com.webobjects.foundation.NSArray;
 
 public class Gumbytext extends TBComponent {
 
   private static final long serialVersionUID = 1L;
-  
+
   //********************************************************************
   //  Constructor : コンストラクタ
   //********************************************************************
   public Gumbytext(WOContext context) {
-        super(context);
-    }
+    super(context);
+  }
 
   //********************************************************************
   //  Methods : メソッド
   //********************************************************************
-private String _day;
+  private String _day;
+  private NSArray<Convention> _tabs;
+
+  public Convention selectedTab;
+  public String Auflistung;
+
+  /* TAB */
+  public NSArray<Convention> tabes() {
+    if (_tabs == null) {
+      Convention a = new Convention("AniNite", 14590);
+      Convention b = new Convention("AkiCon", 3850);
+      Convention c = new Convention("MangaWorld", 8200);
+      _tabs = new NSArray<>(a, b, c);
+    }
+    return _tabs;
+  }
+
   //********************************************************************
   //  Actions : アクション
   //********************************************************************
 
-public String Day() {
-  return _day;
-}
+  public String Day() {
+    return _day;
+  }
 
-public void setDay(String day) {
-  _day = day;
-}
-
+  public void setDay(String day) {
+    _day = day;
+  }
 
   public String myText;
 
   public WOActionResults doSendAction() {
-    System.err.println(myText);
-    return null;
+    System.err.println("// MyText: " + myText +" // Auflistung: "+ Auflistung + "//");
+    return goToMySelfAction();
   }
 
-   
+  public WOActionResults doSampleAction() {
+    return goToMySelfAction();
+  }
+
+  public WOActionResults doTabAction() {
+    log.info("doTabAction wurde aufgerufen.");
+    return goToMySelfAction();
+  }
+
+  //********************************************************************
+  //  Convention Class
+  //********************************************************************
+
+  public class Convention {
+
+    public Convention(String convention, int besucherzahlen) {
+      _convention = convention;
+      _besucherzahlen = besucherzahlen;
+    }
+
+    public void setConvention(String convention) {
+      _convention = convention;
+    }
+
+    public String convention() {
+      return _convention;
+    }
+
+    private String _convention;
+
+    public void setBesucherzahlen(int besucherzahlen) {
+      _besucherzahlen = besucherzahlen;
+    }
+
+    public int besucherzahlen() {
+      return _besucherzahlen;
+    }
+
+    private int _besucherzahlen;
+
+  }
+
+
 }
