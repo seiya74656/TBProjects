2a546702779b855bca144814de76baacb6121ac4
Rene Fuerst <seiya@me.com>
2015-01-10 18:09:35 +0100

    extend superclass Convention

--- HelloBoat/Sources/org/treasureboat/app/components/Akicon.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Akicon.java
@@ -1,46 +1,15 @@
 package org.treasureboat.app.components;
 
-import org.treasureboat.webcore.components.TBComponent;
-
 import com.webobjects.appserver.WOContext;
 
-public class Akicon extends TBComponent {
+public class Akicon extends Convention {
 
   private static final long serialVersionUID = 1L;
 
     public Akicon(WOContext context) {
         super(context);
     }
-    
-    //********************************************************************
-    //  Methods : メソッド
-    //********************************************************************
 
-    //********************************************************************
-    //  Actions : アクション
-    //********************************************************************
-    
-    
-    public String getConvention() {
-      return _convention;
-    }
-
-    public void setConvention(String convention) {
-      _convention = convention;
-    }
-
-    private String _convention;
-
-    public String getRandomtext() {
-      return _randomtext;
-    }
-
-    public void setRandomtext(String Randomtext) {
-      _randomtext = Randomtext;
-    }
-
-    private String _randomtext;
-    
     
     
 }
