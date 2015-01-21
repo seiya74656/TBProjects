a012182e68037b1d33520da3ae47bf517ae1c759
Rene Fuerst <seiya@me.com>
2015-01-21 13:23:33 +0100

    Add localization by Ken to AkiCon.
    Test Localization

--- HelloBoat/Sources/org/treasureboat/app/components/Akicon.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Akicon.java
@@ -1,5 +1,9 @@
 package org.treasureboat.app.components;
 
+import org.treasureboat.webcore.appserver.TBSession;
+import org.treasureboat.webcore.enums.ETBWLanguage;
+import org.treasureboat.webcore.localization.TBWLocalizer;
+
 import com.webobjects.appserver.WOContext;
 
 public class Akicon extends Convention {
@@ -9,5 +13,23 @@ public class Akicon extends Convention {
     public Akicon(WOContext context) {
         super(context);
     }
+    
+    public TBWLocalizer localizer() {
+      TBWLocalizer localizer = super.localizer();
+      
+      localizer = TBWLocalizer.localizerForLanguage(ETBWLanguage.English);
+//      System.err.println(localizer.localizedStringForKey("MyFirstTest"));
+//      
+//      
+//      localizer = TBWLocalizer.localizerForLanguage(ETBWLanguage.Japanese);
+      TBSession.session().setLanguage(ETBWLanguage.English.name());
+      
+      return localizer;
+    }
+    
+    
+    
+    
+    
     
 }
