008a6c301b7e35820f56eab2dae8d4b4ec08d5bb
Rene Fuerst <seiya@me.com>
2015-02-03 11:20:47 +0100

    Filewriter, write gewinnspiel data to a file, with timestamp

--- HelloBoat/Sources/org/treasureboat/app/components/Gewinnspiel.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Gewinnspiel.java
@@ -1,20 +1,18 @@
 package org.treasureboat.app.components;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import org.treasureboat.foundation.TBFString;
-import org.treasureboat.foundation.net.TBFEmailChecker;
+import org.treasureboat.foundation.date.TBFTimestamp;
 import org.treasureboat.webcore.appserver.TBSession;
 import org.treasureboat.webcore.components.TBComponent;
 import org.treasureboat.webcore.enums.ETBWLanguage;
 
 import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOContext;
-import com.webobjects.appserver.WORequest;
-
-import er.javamail.ERMailDeliveryHTML;
-import er.javamail.ERMailDeliveryPlainText;
 
 public class Gewinnspiel extends TBComponent {
 
@@ -63,94 +61,110 @@ public class Gewinnspiel extends TBComponent {
     return goToMySelfAction();
   }
 
-  //  public String elementID() {
-  //    if (TBFString.stringIsNullOrEmpty(_elementID)) {
-  //      _elementID = context().elementID();
-  //    }
-  //    return _elementID;
-  //  }
-  //  private String _elementID;
+  // public String elementID() {
+  // if (TBFString.stringIsNullOrEmpty(_elementID)) {
+  // _elementID = context().elementID();
+  // }
+  // return _elementID;
+  // }
+  // private String _elementID;
 
   // Response Request Loop
 
-  //  @Override
-  //  public void awake() {
-  //    super.awake();
-  //  }
-  //  
-  //  @Override
-  //  public void takeValuesFromRequest(WORequest request, WOContext context) {
-  //    super.takeValuesFromRequest(request, context);
-  //  }
+  // @Override
+  // public void awake() {
+  // super.awake();
+  // }
+  //
+  // @Override
+  // public void takeValuesFromRequest(WORequest request, WOContext context) {
+  // super.takeValuesFromRequest(request, context);
+  // }
 
-  //  @Override
-  //  public WOActionResults invokeAction(WORequest request, WOContext context) {
-  //    return super.invokeAction(request, context);
-  //  }
-  //  
-  //  @Override
-  //  public void appendToResponse(WOResponse response, WOContext context) {
-  //    super.appendToResponse(response, context);
-  //    _elementID = null;
-  //  }
-  //  
-  //  @Override
-  //  public void sleep() {
-  //    super.sleep();
-  //  }
+  // @Override
+  // public WOActionResults invokeAction(WORequest request, WOContext context) {
+  // return super.invokeAction(request, context);
+  // }
+  //
+  // @Override
+  // public void appendToResponse(WOResponse response, WOContext context) {
+  // super.appendToResponse(response, context);
+  // _elementID = null;
+  // }
+  //
+  // @Override
+  // public void sleep() {
+  // super.sleep();
+  // }
 
-  public WOActionResults doMitmachenAction() 
-  // TODO
-      throws AddressException, MessagingException {
+  private void writertoFile(String name, String antwort, String email, Boolean agb) throws IOException {
+    // Datei erstellen und beschreiben
 
-//    WOContext context = context();
-//    System.err.println("WOContext --> " + context);
-//    System.err.println("WORequest --> " + context.request());
-//    System.err.println("WOResponse --> " + context.response());
+    try {
+      FileWriter writer = new FileWriter("gewinnerliste.txt", true);
+      writer.append("Date: " + timestamp() + ", \t Name: " + deinname + ", Email: " + email + ", Antwort: " + antwort + " AGB: " + agb + "\n");
+      writer.close();
+      log.info(">>>> Daten wurden in die Datei geschrieben!! <<<<");
+    }
+    catch (FileNotFoundException | UnsupportedEncodingException e) {
+      log.info("File not found");
+      e.printStackTrace();
+    }
+  }
+  // Datei erstellen und beschreiben
 
-//    Other way to get Information from html form context antwortX!
 
-//    WORequest request = context.request();
-//    System.err.println(request.formValueKeys());
-//    System.err.println("=== " + request.formValuesForKey("antwortX"));
+  public WOActionResults doMitmachenAction() throws IOException {
+    // WOContext context = context();
+    // System.err.println("WOContext --> " + context);
+    // System.err.println("WORequest --> " + context.request());
+    // System.err.println("WOResponse --> " + context.response());
+
+    // Other way to get Information from html form context antwortX!
+
+    // WORequest request = context.request();
+    // System.err.println(request.formValueKeys());
+    // System.err.println("=== " + request.formValuesForKey("antwortX"));
 
     // Email
 
-//    System.err.println(TBFEmailChecker.emailCharFullCheck("rene@yahoo.com"));
-//    System.err.println(TBFEmailChecker.emailCharFullCheck("rene@yahoo.c"));
-      
-    
+    // System.err.println(TBFEmailChecker.emailCharFullCheck("rene@yahoo.com"));
+    // System.err.println(TBFEmailChecker.emailCharFullCheck("rene@yahoo.c"));
 
     if (!agb || TBFString.stringIsNullOrEmpty(deinname) || TBFString.stringIsNullOrEmpty(email) || TBFString.stringIsNullOrEmpty(antwort) || !isValidEmailAddress(email)) {
       log.info("Mitmachen fehlgeschlagen!!! AGB: {}, Name: {}, Email: {}, Antwort: {}", agb, deinname, email, antwort);
     }
     else {
       log.info("Mitmachen wurde aufgerufen. AGB: {}, Name: {}, Email: {}, Antwort: {}", agb, deinname, email, antwort);
+      writertoFile(deinname, antwort, email, agb);
 
       /*
        * ERJavaMail sendmail = new ERJavaMail();
        * sendmail.isValidEmail(email);
        */
-      ERMailDeliveryPlainText mail = new ERMailDeliveryPlainText();
-      mail.setTextContent("Hello TreasureBoat!");
-      try {
-
-          mail.setSubject("TreasureBoat Test Mail!");
-          mail.sendMail();
-
-        mail.newMail();
-        mail.setFromAddress("renate@ebone.at");
-        mail.setReplyToAddress("seiya@me.com");
-        mail.setSubject("Test ueberschrift");
-        mail.setToAddress("seiya74656@gmail.com");
-        mail.sendMail();
-      } catch (Exception e) {
-        System.err.println("Mailsenden fehlgeschlagen: " + e);
-      }
-
+      // ERMailDeliveryPlainText mail = new ERMailDeliveryPlainText();
+      // mail.setTextContent("Hello TreasureBoat!");
+      // try {
+      //
+      // mail.setSubject("TreasureBoat Test Mail!");
+      // mail.sendMail();
+      //
+      // mail.newMail();
+      // mail.setFromAddress("renate@ebone.at");
+      // mail.setReplyToAddress("seiya@me.com");
+      // mail.setSubject("Test ueberschrift");
+      // mail.setToAddress("seiya74656@gmail.com");
+      // mail.sendMail();
+      // } catch (Exception e) {
+      // System.err.println("Mailsenden fehlgeschlagen: " + e);
+      // }
 
     }
     return goToMySelfAction();
+  }
+
+  public TBFTimestamp timestamp() {
+    return new TBFTimestamp();
   }
 
 }
