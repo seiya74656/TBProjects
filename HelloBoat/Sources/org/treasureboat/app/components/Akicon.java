22afd899e69cae50b3aa67e473d5c44ac8a262ff
Ken ISHIMOTO <ken@ksroom.com>
2015-02-03 17:41:50 +0100

    grundgerüst

--- HelloBoat/Sources/org/treasureboat/app/components/Akicon.java
+++ HelloBoat/Sources/org/treasureboat/app/components/Akicon.java
@@ -1,35 +1,64 @@
 package org.treasureboat.app.components;
 
+import org.treasureboat.app.eo.ToDo;
 import org.treasureboat.webcore.appserver.TBSession;
 import org.treasureboat.webcore.enums.ETBWLanguage;
 import org.treasureboat.webcore.localization.TBWLocalizer;
 
+import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSArray;
 
 public class Akicon extends Convention {
 
   private static final long serialVersionUID = 1L;
 
-    public Akicon(WOContext context) {
-        super(context);
-    }
-    
-    public TBWLocalizer localizer() {
-      TBWLocalizer localizer = super.localizer();
-      
-      localizer = TBWLocalizer.localizerForLanguage(ETBWLanguage.English);
-//      System.err.println(localizer.localizedStringForKey("MyFirstTest"));
-//      
-//      
-//      localizer = TBWLocalizer.localizerForLanguage(ETBWLanguage.Japanese);
-      TBSession.session().setLanguage(ETBWLanguage.English.name());
-      
-      return localizer;
-    }
-    
-    
-    
-    
-    
-    
+  public Akicon(WOContext context) {
+    super(context);
+  }
+
+  public TBWLocalizer localizer() {
+    TBWLocalizer localizer = super.localizer();
+
+    localizer = TBWLocalizer.localizerForLanguage(ETBWLanguage.English);
+    //      System.err.println(localizer.localizedStringForKey("MyFirstTest"));
+    //      
+    //      
+    //      localizer = TBWLocalizer.localizerForLanguage(ETBWLanguage.Japanese);
+    TBSession.session().setLanguage(ETBWLanguage.English.name());
+
+    return localizer;
+  }
+
+
+  //********************************************************************
+  //  
+  //********************************************************************
+
+  public NSArray<ToDo> allTodos() {
+
+    return null;
+  }
+
+  public NSArray<ToDo> selectionTodos() {
+
+    return null;
+  }
+
+  public ToDo selectedTodo() {
+
+    return null;
+  }
+
+  public ToDo oneTodo;
+
+
+  public WOActionResults doSaveAction() {
+
+
+
+    return goToMySelfAction();
+  }
+
+
 }
