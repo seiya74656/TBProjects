3294d2b408a436aab088533001b15ccee15310f0
Rene Fuerst <seiya@me.com>
2015-02-15 22:03:18 +0100

    Add Aninite CSS and Bootstrap

---
+++ TBAniNite/Sources/org/treasureboat/app/components/AniNiteHome.java
@@ -0,0 +1,16 @@
+package org.treasureboat.app.components;
+
+import org.treasureboat.webcore.components.TBComponent;
+
+import com.webobjects.appserver.WOContext;
+
+public class AniNiteHome extends TBComponent {
+
+  private static final long serialVersionUID = 1L;
+
+  public AniNiteHome(WOContext context) {
+    super(context);
+    // TODO Auto-generated constructor stub
+  }
+
+}
