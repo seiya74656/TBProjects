2109b59eef2764c6f54405181616f1340aa5ddaa
Rene Fuerst <seiya@me.com>
2015-03-08 21:22:41 +0100

    init Statistik Austria Barcode Scanner

---
+++ TBLarenta/Sources/org/treasureboat/app/components/StatistikAustria.java
@@ -0,0 +1,17 @@
+package org.treasureboat.app.components;
+
+import com.webobjects.appserver.WOContext;
+
+public class StatistikAustria extends Main {
+
+  /**
+   * For Barcode Scanner via iPhone WebApp
+   */
+  private static final long serialVersionUID = 1L;
+
+  public StatistikAustria(WOContext context) {
+    super(context);
+    // TODO Auto-generated constructor stub
+  }
+
+}
