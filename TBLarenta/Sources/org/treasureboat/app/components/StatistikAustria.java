70f11906cadbd88ec085fe4712467e66b08a9640
Rene Fuerst <seiya@me.com>
2015-03-10 14:02:34 +0100

    insert for Barcodecheck working.

--- TBLarenta/Sources/org/treasureboat/app/components/StatistikAustria.java
+++ TBLarenta/Sources/org/treasureboat/app/components/StatistikAustria.java
@@ -1,7 +1,13 @@
 package org.treasureboat.app.components;
 
+import org.treasureboat.app.eo.BarcodeRechnungTBL;
+
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOActionResults;
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSTimestamp;
+
+import er.extensions.eof.ERXEC;
 
 public class StatistikAustria extends Main {
 
@@ -11,14 +17,26 @@ public class StatistikAustria extends Main {
   private static final long serialVersionUID = 1L;
 
   public String barcode;
-  
+
   public StatistikAustria(WOContext context) {
     super(context);
   }
 
   public WOActionResults barcodecheck() {
+
+     EOEditingContext ec = ERXEC.newEditingContext();
+     BarcodeRechnungTBL newBaRechnungTBL = BarcodeRechnungTBL.createAndInsertInstance(ec);
+     int intbarcode = Integer.parseInt(barcode);
+     newBaRechnungTBL.setBarcode(intbarcode);
+     newBaRechnungTBL.setScanned(timestamp());
+    
+     ec.saveChanges();
     log.info("Barcode is:" + barcode);
     return goToMySelfAction();
+  }
+
+  public NSTimestamp timestamp() {
+    return new NSTimestamp();
   }
 
 }
