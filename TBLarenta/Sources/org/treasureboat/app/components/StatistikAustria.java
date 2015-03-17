2381297e7eb85256e1704b293c3ac598ac3bd87e
Ken Ishimoto <ken@ksroom.com>
2015-03-17 22:05:13 +0100

    fix statisik

--- TBLarenta/Sources/org/treasureboat/app/components/StatistikAustria.java
+++ TBLarenta/Sources/org/treasureboat/app/components/StatistikAustria.java
@@ -1,14 +1,15 @@
 package org.treasureboat.app.components;
 
+import org.treasureboat.app.eo.BarcodeCheckTBL;
 import org.treasureboat.app.eo.BarcodeRechnungTBL;
+import org.treasureboat.foundation.TBFString;
+import org.treasureboat.foundation.date.TBFTimestamp;
 import org.treasureboat.webcore.components.TBComponent;
 
-import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOActionResults;
-import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSTimestamp;
-
-import er.extensions.eof.ERXEC;
 
 public class StatistikAustria extends TBComponent {
 
@@ -26,9 +27,9 @@ public class StatistikAustria extends TBComponent {
   public boolean barcodeReTBL(int intbarcode) {
 
     BarcodeRechnungTBL testb = BarcodeRechnungTBL.fetchBarcodeRechnungTBL(editingContext(), BarcodeRechnungTBL.BARCODE.eq(intbarcode));
-    
+
     final String ObjecttoString = testb.toString(); // Wandelt die Aufgabe in einen String um , um mit parseInt zu convertieren
-    
+
     Integer testcode = Integer.parseInt(ObjecttoString); //Convertiert den String in Int
     if (testcode.equals(intbarcode)) { //vergleicht ob der Integer mit der eingangs intbarcode überein stimmt
       return true;
@@ -36,29 +37,57 @@ public class StatistikAustria extends TBComponent {
       return false;
     }
   }
-  
-// NOT WORKING , try to generate a NSArray with intbarcode, and count Array, if array higher als 0 , check is true
-//  public NSArray<BarcodeRechnungTBL> allBarcodeRechnungTBLs(int intbarcode) {
-//    return BarcodeRechnungTBL.fetchAllBarcodeRechnungTBLs(editingContext(), BarcodeRechnungTBL.BARCODE.eq(intbarcode));
-//  }
-  
+
+  // NOT WORKING , try to generate a NSArray with intbarcode, and count Array, if array higher als 0 , check is true
+  //  public NSArray<BarcodeRechnungTBL> allBarcodeRechnungTBLs(int intbarcode) {
+  //    return BarcodeRechnungTBL.fetchAllBarcodeRechnungTBLs(editingContext(), BarcodeRechnungTBL.BARCODE.eq(intbarcode));
+  //  }
+
   public WOActionResults barcodecheck() {
-    
-     EOEditingContext ec = ERXEC.newEditingContext();
-     BarcodeRechnungTBL newBaRechnungTBL = BarcodeRechnungTBL.createAndInsertInstance(ec);
-     int intbarcode = Integer.parseInt(barcode);
-     newBaRechnungTBL.setBarcode(intbarcode);
-     newBaRechnungTBL.setScanned(timestamp());
-    
-     ec.saveChanges();
-    log.info("Barcode is:" + barcode);
-    
-    if (barcodeReTBL(intbarcode)) { 
-      log.info("Barcodevergleich ist TRUE");
-    } else {
-      log.info("Barcodevergleich ist FALSE");
+
+
+    if (!TBFString.stringIsNullOrEmpty(barcode)) {
+      TBFTimestamp now = new TBFTimestamp();
+
+      int intBarCode = TBFString.digitStringAsInt(barcode);
+
+      if (intBarCode > 0) {
+        
+        NSArray<BarcodeCheckTBL> barcodeChecks = BarcodeCheckTBL.fetchBarcodeCheckTBLs(editingContext(), BarcodeCheckTBL.BARCODEVALID.eq(intBarCode).and(BarcodeCheckTBL.VALID_DATE.after(now)), BarcodeCheckTBL.VALID_DATE.asc().array());
+        if (barcodeChecks.count() != 0) {
+//          BarcodeCheckTBL barcodeCheck = barcodeChecks.firstObject();
+          @SuppressWarnings("unused")
+          BarcodeRechnungTBL barcodeRechnung = BarcodeRechnungTBL.createBarcodeRechnungTBL(editingContext(), intBarCode, now.timestamp());      
+          editingContext().saveChanges();
+        } else {
+          // show error
+          
+        }
+      }
     }
-    
+
+
+
+
+
+
+
+
+
+    //     EOEditingContext ec = ERXEC.newEditingContext();
+    //     BarcodeRechnungTBL newBaRechnungTBL = BarcodeRechnungTBL.createAndInsertInstance(ec);
+    //     newBaRechnungTBL.setBarcode(intbarcode);
+    //     newBaRechnungTBL.setScanned(timestamp());
+    //    
+    //     ec.saveChanges();
+    //    log.info("Barcode is:" + barcode);
+    //    
+    //    if (barcodeReTBL(intbarcode)) { 
+    //      log.info("Barcodevergleich ist TRUE");
+    //    } else {
+    //      log.info("Barcodevergleich ist FALSE");
+    //    }
+
     return goToMySelfAction();
   }
 
