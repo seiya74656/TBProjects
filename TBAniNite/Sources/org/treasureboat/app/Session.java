a5027931b43db39e310c365bffebf01f22cd5533
Ken Ishimoto <ken@ksroom.com>
2015-03-26 20:35:56 +0100

    fix Session

--- TBAniNite/Sources/org/treasureboat/app/Session.java
+++ TBAniNite/Sources/org/treasureboat/app/Session.java
@@ -1,43 +1,37 @@
 package org.treasureboat.app;
 
-import org.treasureboat.app.navigation.NavigationController;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.treasureboat.webcore.appserver.TBSession;
+import org.treasureboat.app.navigation.NavigationController;
 import org.treasureboat.foundation.constants.TBFKnownLookNames;
-import org.treasureboat.webcore.appserver.navbar.handler.TBNavigationHandler;
+import org.treasureboat.webcore.appserver.TBSession;
 
 public class Session extends TBSession {
-	/**
-	 * Do I need to update serialVersionUID?
-	 * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
-	 * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
-	 */
-	private static final long serialVersionUID = 1L;
-   
-	/** 
-	 * <a href="http://wiki.wocommunity.org/display/documentation/Wonder+Logging">new org.slf4j.Logger</a> 
-	 */
-	static final Logger log = LoggerFactory.getLogger(Session.class);
-   
-	//********************************************************************
-	//  Constructor : コンストラクター
-	//********************************************************************
+  /**
+   * Do I need to update serialVersionUID?
+   * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
+   * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
+   */
+  private static final long serialVersionUID = 1L;
 
-	public Session() {
-		super();
+  /** 
+   * <a href="http://wiki.wocommunity.org/display/documentation/Wonder+Logging">new org.slf4j.Logger</a> 
+   */
+  static final Logger log = LoggerFactory.getLogger(Session.class);
 
-		// Set the Navigation Controller
-		if(navController() == null) {
-			setNavController(new NavigationController(this));
-		}
+  //********************************************************************
+  //  Constructor : コンストラクター
+  //********************************************************************
 
-		// Menu Handler メニュー・ハンドラー
-		TBNavigationHandler.setRoot(this);
-	
-		// set the default Look
-		setCurrentD2WLook(TBFKnownLookNames.GUMBY);
-	}
+  public Session() {
+    super();
+
+    // Set the Navigation Controller
+    if(navController() == null) {
+      setNavController(new NavigationController(this));
+    }
+
+    // set the default Look
+    setCurrentD2WLook(TBFKnownLookNames.GUMBY);
+  }
 }
